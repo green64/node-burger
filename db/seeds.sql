936fe368aac45efe4a082bdced8e335f1799dbb9
Samantha McCall <samanthaadams.biz@gmail.com>
2018-04-28 09:12:22 -0400

    adding all files, servers, routes, packages

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Cincinnati Slider');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Perfect Patty Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheese', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Masterpiece');
+INSERT INTO burgers (burger_name) VALUES ('Chef Steve Special');

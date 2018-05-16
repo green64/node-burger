b899a75d9a33ff7b3658670752505d935087b793
Samantha McCall <samanthaadams.biz@gmail.com>
2018-05-16 18:57:56 -0500

    updating production server info in jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,33 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Newjob18",
-  database: "burgers_db"
+  // port: 3306,
+  // host: "localhost",
+  // user: "root",
+  // password: "Newjob18",
+  // database: "burgers_db"
+
+  "development": {
+    "user": "root",
+    "password": "Newjob18",
+    "database": "burgers_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "Newjob18",
+    "database": "app_entries",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "port": 3306,
+    "username": "xezr07e1hqvwm469",
+    "password": "ac56l36sufy08380",
+    "database": "tfxnuofy7u5nuy95	",
+    "host": "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+  }
 });
 
 // Make connection.

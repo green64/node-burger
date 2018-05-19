84958ab04f325e0a9cb4e36aac9c8d191c643137
Samantha McCall <samanthaadams.biz@gmail.com>
2018-05-18 19:35:24 -0500

    updating app with working files

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,15 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
+  port: 3306,
+  username: "xezr07e1hqvwm469",
+  password: "qf8kbv0lvrliscx2",
+  database: "tfxnuofy7u5nuy95",
+  host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
   // host: "localhost",
   // user: "root",
   // password: "Newjob18",
   // database: "burgers_db"
-
-
-      port: 3306,
-      username: "xezr07e1hqvwm469",
-      password: "ac56l36sufy08380",
-      database: "tfxnuofy7u5nuy95",
-      host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-
 });
 
 // Make connection.

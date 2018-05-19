37ac09cc8c8480c5688cf21ef3f743248c7b90e0
Samantha McCall <samanthaadams.biz@gmail.com>
2018-05-18 20:16:34 -0500

    updating app with working files

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
+  // port: 3306,
   username: "xezr07e1hqvwm469",
   password: "qf8kbv0lvrliscx2",
   database: "tfxnuofy7u5nuy95",
